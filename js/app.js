8826b554cc577bab49a335db6f09bb8378fc9668
Brian Crowe <brian.j.crowe@protonmail.com>
2025-06-25 23:06:40 -0400

    rewrite while as for loop

--- js/app.js
+++ js/app.js
@@ -3,31 +3,27 @@ const getRandomNumber = (upper) => Math.floor(Math.random() * upper) + 1
 const main = document.querySelector('main')
 const randomNumber = getRandomNumber(10)
 let guess
-let guessCount = 0
+let message = ''
 
 // TODO: Use a loop to create a number guessing game
 //  1) Ask the user to enter a number and assign that value to the `guess` variable
-guess = parseInt(
-  prompt('I am thinking of a number between 1 and 10. What is it?')
-)
-guessCount++
 
 //  2) End the loop when the user's guess matches the random number
 
-while (guessCount < 10) {
-  guess = parseInt(prompt('Incorrect. Please guess again:'))
-  if (guess === randomNumber) {
-    break
+//Convert while to for loop
+for (let i = 1; i <= 10; i++) {
+  if (i === 1) {
+    guess = prompt('I am thinking of a number between 1 and 10. What is it?')
+  } else {
+    guess = prompt('Incorrect. Please guess again.')
   }
-  guessCount++
+  if (parseInt(guess) === randomNumber) {
+    message = `<h1>You guessed the number! It was ${randomNumber}.</h1><h2>It only took you ${i} guesses.</h2>`
+    break
+  } else {
+    message = `<h1>Womp Womp! You ran out of guesses. It was ${randomNumber}.</h1>`
+  }
 }
-
-const correctGuess = `<h1>You guessed the number! It was ${randomNumber}.</h1><h2>It only took you ${guessCount} guesses.</h2>`
-const incorrectGuess = `<h1>Womp Womp! You ran out of guesses. It was ${randomNumber}.</h1>`
 
 //  3) Display a message letting the user know they guessed the number
-if (guess === randomNumber) {
-  main.innerHTML = correctGuess
-} else {
-  main.innerHTML = incorrectGuess
-}
+main.innerHTML = message
